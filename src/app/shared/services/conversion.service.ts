5f42e4a118d732a7eb3d0e143e053ea20960e859
Nikolay Varlamov <varlamow.col@yahoo.com>
2021-01-18 17:33:44 +0300

    child_process importing

--- src/app/shared/services/conversion.service.ts
+++ src/app/shared/services/conversion.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Document } from '../../models/document';
 import * as path from 'path';
+import * as childProcess from 'child_process';
+import { ElectronService } from '../../core/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversionService {
   path: typeof path;
+  childProcess: typeof childProcess;
 
-  constructor() {
-    this.path = window.require('path');
+  constructor(private electron: ElectronService) {
+    if (this.electron.isElectron) {
+      this.path = window.require('path');
+      this.childProcess = window.require('child_process');
+    }
   }
 
   private getFileType(path: string): string {
-    return this.path.extname(path);
+    if (this.electron.isElectron) {
+      return this.path.extname(path);
+    }
   }
 
   public convertDocument(path: string): Document {
+    if (this.electron.isElectron) {
+
+    }
   }
 
 }
