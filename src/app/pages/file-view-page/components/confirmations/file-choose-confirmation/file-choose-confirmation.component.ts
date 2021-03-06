e8966d83272cfb22178f872bf60c0cd60c4b29d0
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-03-06 23:50:42 +0300

    add file choose confirmation layout

--- src/app/pages/file-view-page/components/confirmations/file-choose-confirmation/file-choose-confirmation.component.ts
+++ src/app/pages/file-view-page/components/confirmations/file-choose-confirmation/file-choose-confirmation.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { DocumentService } from '../../../services';
 
 @Component({
   selector: 'app-file-choose-confirmation',
   templateUrl: './file-choose-confirmation.component.html',
   styleUrls: ['./file-choose-confirmation.component.scss']
 })
-export class FileChooseConfirmationComponent implements OnInit {
+export class FileChooseConfirmationComponent {
 
-  constructor() { }
+  @Output('cancel-click')
+  public readonly cancelClickEmitter = new EventEmitter<void>();
 
-  ngOnInit(): void {
-  }
+  @Output('select-click')
+  public readonly selectClickEmitter = new EventEmitter<void>(); // FIXME: output type
+
+  constructor(
+    public readonly documentService: DocumentService,
+  ) {}
 
 }
