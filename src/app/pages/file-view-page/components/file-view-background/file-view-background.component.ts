fbbbd30a1c99487923ab5febead289124143e4ad
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-04-03 20:12:15 +0300

    change empty file view page styles

--- src/app/pages/file-view-page/components/file-view-background/file-view-background.component.ts
+++ src/app/pages/file-view-page/components/file-view-background/file-view-background.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-file-view-background',
   templateUrl: './file-view-background.component.html',
   styleUrls: ['./file-view-background.component.scss']
 })
-export class FileViewBackgroundComponent implements OnInit {
+export class FileViewBackgroundComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
