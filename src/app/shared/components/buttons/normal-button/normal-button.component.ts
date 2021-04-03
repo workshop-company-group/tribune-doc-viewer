5716359d27dbc43413f2f28d8714cbc2fde375bc
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-04-03 19:08:55 +0300

    change main menu page styles

--- src/app/shared/components/buttons/normal-button/normal-button.component.ts
+++ src/app/shared/components/buttons/normal-button/normal-button.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, } from '@angular/core';
 
 @Component({
   selector: 'app-normal-button',
   templateUrl: './normal-button.component.html',
   styleUrls: ['./normal-button.component.scss']
 })
-export class NormalButtonComponent implements OnInit {
+export class NormalButtonComponent {
+
+  @Output('button-click')
+  public clickEmitter = new EventEmitter<void>();
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
