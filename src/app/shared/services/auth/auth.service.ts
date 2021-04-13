6fbbc5a390626616b00c57410559001b4b7f394b
Nikolay Varlamov <varlamow.col@yahoo.com>
2021-04-13 21:46:59 +0300

    auth service init

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as fs from 'fs';
+import * as jsonfile from 'jsonfile'
+import { Auth } from './auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  fs: typeof fs;
+  jsonfile: typeof jsonfile;
 
-  constructor() { }
+  constructor() {
+    this.fs = window.require('fs');
+    this.fs = window.require('jsonfile');
+  }
 
-  public createU
+  public setPassword(password: string): void {
+    const auth: Auth = { password };
+    this.jsonfile.writeFileSync('auth.json', auth);
+  }
+
+  public passwordIsValid(password: string): boolean {
+    const auth: Auth = this.jsonfile.readFileSync('auth.json');
+    return password === auth.password;
+  }
 }
