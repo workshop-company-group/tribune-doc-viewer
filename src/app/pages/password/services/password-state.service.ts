a467916d5f7b917c0bdbd722f6ce9731750dd8da
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-04-24 18:05:06 +0300

    add password state service

---
+++ src/app/pages/password/services/password-state.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject, } from 'rxjs';
+
+type PasswordPageState = null | 'logout' | 'settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PasswordStateService {
+
+  private pageStateSubject = new BehaviorSubject<PasswordPageState>(null);
+
+  public pageStateObservable = this.pageStateSubject.asObservable();
+
+  constructor() { }
+
+  public get pageState(): PasswordPageState {
+    return this.pageStateSubject.value;
+  }
+
+  public set pageState(value: PasswordPageState) {
+    this.pageStateSubject.next(value);
+  }
+
+}
