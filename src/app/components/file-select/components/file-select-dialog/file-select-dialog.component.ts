d66197214dfe9e19c60d21199fa789ea212ab163
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-04-27 11:18:49 +0300

    add drives in file select dialog

--- src/app/components/file-select/components/file-select-dialog/file-select-dialog.component.ts
+++ src/app/components/file-select/components/file-select-dialog/file-select-dialog.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Output, } from '@angular/core';
 
+import { from, Observable, } from 'rxjs';
+
+import { FileSystemService, } from '../../../../shared/services';
+
+import { Drive, Mountpoint, } from '../../../../shared/models';
+
 @Component({
   selector: 'app-file-select-dialog',
   templateUrl: './file-select-dialog.component.html',
@@ -10,12 +16,32 @@ export class FileSelectDialogComponent {
   @Output('close-click')
   public readonly closeEmitter = new EventEmitter<void>();
 
-  constructor() { }
+  public mountpoints: Observable<Mountpoint[]> =
+    from(this.listMountpoints());
+
+  constructor(
+    public readonly fileSystem: FileSystemService,
+  ) { }
 
   public async openFiles(): Promise<void> {
+    const mountpoints = await this.listMountpoints();
+    console.log(mountpoints);
     // TODO: open files
     // await DocumentService.open(path)
     this.closeEmitter.emit();
+  }
+
+  public async listMountpoints(): Promise<Mountpoint[]> {
+    const mountpoints: Mountpoint[] = [];
+    const drives: Drive[] = await this.fileSystem.listDrives();
+    for (const drive of drives) {
+      for (const mountpoint of drive.mountpoints) {
+        if (this.fileSystem.dirExists(mountpoint.path)) {
+          mountpoints.push(mountpoint);
+        }
+      }
+    }
+    return mountpoints;
   }
 
 }
