944d1f0e42482082fd449a221b02c95b6ae25581
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-06-13 12:30:40 +0300

    license check

--- src/app/shared/services/license/license.service.ts
+++ src/app/shared/services/license/license.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { LicenseError } from '../exceptions';
 import { ElectronService } from '../../../core/services';
 import * as fs from 'fs';
+import { time } from 'systeminformation';
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +18,14 @@ export class LicenseService {
 
   private readonly defaultPath: string = 'license.key';
 
-  private saveLicenseToFile(key: string) {
+  private saveLicenseToFile(key: string): void {
     fs.writeFile(this.defaultPath, key, {flag: 'wx'}, function (err) {
       if (err) throw err;
     });
+  }
+
+  private readLicenseFromFile(): string {
+    return fs.readFileSync(this.defaultPath).toString();
   }
 
   public async IsLicenseKeyValid(key: string): Promise<boolean> {
@@ -36,6 +41,7 @@ export class LicenseService {
   public async Activate(key: string): Promise<boolean> {
     if (await this.IsLicenseKeyValid(key)) {
       this.saveLicenseToFile(key);
+      console.log('License: ', this.readLicenseFromFile());
       return true;
     }
     return false;
