65cd6bd266736ed44976e0cad051224510114d71
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-06-22 00:30:36 +0300

    index import upd

--- src/app/shared/services/exceptions/index.ts
+++ src/app/shared/services/exceptions/index.ts
@@ -1,5 +1,4 @@
 export { WindowError } from './window-error';
 export { RecorderError } from './recorder-error';
 export { FileSystemError } from './file-system-error';
-export { LicenseError } from './license-error';
 export { LocalesError } from './locales-error';
