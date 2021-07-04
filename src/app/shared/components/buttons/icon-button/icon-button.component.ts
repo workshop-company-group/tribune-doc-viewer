e80e5cbf1ed60de80557c09d6a060ae028558ab7
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-07-04 23:25:29 +0300

    TRIB-65 add icon button animation

---
+++ src/app/shared/components/buttons/icon-button/icon-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, EventEmitter, Output, } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-button',
+  templateUrl: './icon-button.component.html',
+  styleUrls: ['./icon-button.component.scss']
+})
+export class IconButtonComponent {
+
+  @Input('src')
+  public iconSource: string;
+
+  @Input()
+  public readonly disabled = false;
+
+  @Output('button-click')
+  public clickEmitter = new EventEmitter<void>();
+
+  constructor() { }
+
+}
