313603bb83f1ee5719f9727b8de3665b3bf0ee20
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-08-24 22:41:48 +0300

    electron service fix

--- src/app/core/services/electron/electron.service.ts
+++ src/app/core/services/electron/electron.service.ts
@@ -1,9 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
-/* eslint-disable @typescript-eslint/prefer-optional-chain */
-/* eslint-disable @typescript-eslint/lines-between-class-members */
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
-// should not edit something here because it's core electron service
-
 import { Injectable } from '@angular/core';
 
 // If you import a module but never use any of
@@ -19,14 +13,18 @@ import * as fs from 'fs';
   providedIn: 'root',
 })
 export class ElectronService {
-  ipcRenderer: typeof ipcRenderer;
-  webFrame: typeof webFrame;
-  remote: typeof remote;
-  childProcess: typeof childProcess;
-  fs: typeof fs;
+  public ipcRenderer: typeof ipcRenderer;
 
-  get isElectron(): boolean {
-    return !!(window && window.process && window.process.type);
+  public webFrame: typeof webFrame;
+
+  public remote: typeof remote;
+
+  public childProcess: typeof childProcess;
+
+  public fs: typeof fs;
+
+  public get isElectron(): boolean {
+    return !!(window.process.type);
   }
 
   constructor() {
