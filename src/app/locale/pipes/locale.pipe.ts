04767613405675b517bce141408d5bd4d4dc1e4d
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-08-24 18:31:32 +0300

    TRIB-70 lint locale module

--- src/app/locale/pipes/locale.pipe.ts
+++ src/app/locale/pipes/locale.pipe.ts
@@ -18,12 +18,13 @@ export class LocalePipe implements PipeTransform {
 
   public transform(phrase: string, ...args: string[]): Observable<string> {
     return this.settings.localeSubject.pipe(
-      map((locale) => this.locales.getLocaledPhrase(phrase, locale)),
-      map((phrase) => {
-        for (const index in args) {
-          phrase = phrase.replace(`{${index}}`, args[index]);
-        }
-        return phrase;
+      map(locale => this.locales.getLocaledPhrase(phrase, locale)),
+      map(phrase => {
+        let filledPhrase = phrase;
+        args.forEach((arg, index) => {
+          filledPhrase = phrase.replace(`{${index}}`, args[index]);
+        });
+        return filledPhrase;
       }),
     );
   }
