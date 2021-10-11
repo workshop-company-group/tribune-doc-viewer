1f365f6dae5e528fe78f04631da27187a1165193
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-10-11 12:56:41 +0300

    TRIB-68 refactor temp commit

--- src/app/shared/components/backdrop/backdrop.component.ts
+++ src/app/shared/components/backdrop/backdrop.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-backdrop',
@@ -9,6 +14,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class BackdropComponent {
 
   @Input()
+  @HostBinding('class.opened')
   public opened = false;
 
   constructor() { }
